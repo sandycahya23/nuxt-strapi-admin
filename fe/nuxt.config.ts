87acc900042334d63ec37017a2bf90327f535402
sandy cahya <sandy.cahya.23@gmail.com>
2022-09-10 21:53:05 +0700

    init strapi

---
+++ fe/nuxt.config.ts
@@ -0,0 +1,20 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/strapi'
+  ],
+  // https://content.nuxtjs.org/api/configuration
+  content: {
+  },
+  // https://strapi.nuxtjs.org/setup
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt'
+  }  
+})
